babb04102f4136c9e1283971c1e5796d4b3d8833
codeinstack <codeinstack@gmail.com>
2020-08-14 11:23:21 +0530

    update our menuitems to navigate withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+
+// In Future will be deleting the below pages data functional component. at present just for checking
+//Deletions starts
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+const JacketsPage = () => (
+  <div>
+    <h1>JacketsPage</h1>
+  </div>
+);
+const SneakersPage = () => (
+  <div>
+    <h1>SneakersPage</h1>
+  </div>
+);
+const WomensPage = () => (
+  <div>
+    <h1>WomensPage</h1>
+  </div>
+);
+const MensPage = () => (
+  <div>
+    <h1>MensPage</h1>
+  </div>
+);
+//Deletion ends here in future
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route path="/shop/hats" component={HatsPage} />
+        <Route path="/shop/jackets" component={JacketsPage} />
+        <Route path="/shop/sneakers" component={SneakersPage} />
+        <Route path="/shop/womens" component={WomensPage} />
+        <Route path="/shop/mens" component={MensPage} />
+        <Route path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 }
